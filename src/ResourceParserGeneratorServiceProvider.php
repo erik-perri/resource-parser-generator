cb20b8a4dbe3c4ecb05c03f3ba88a356af98d6ef
Erik Perri <erik.perri@gmail.com>
2023-04-19 19:26:16 -0400

    initial commit

---
+++ src/ResourceParserGeneratorServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ResourceParserGenerator;
+
+use Illuminate\Support\ServiceProvider;
+
+class ResourceParserGeneratorServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+}

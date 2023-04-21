6d4bc1cecd23fc442df1cc66e0de716138a38ac9
Erik Perri <erik.perri@gmail.com>
2023-04-21 00:28:33 -0400

    update: Added basic shape generation

--- src/Commands/GenerateResourceParserCommand.php
+++ src/Commands/GenerateResourceParserCommand.php
@@ -5,41 +5,121 @@ declare(strict_types=1);
 namespace ResourceParserGenerator\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
 use ResourceParserGenerator\Filesystem\ClassFileFinder;
+use ResourceParserGenerator\Parsers\PhpParser\ClassMethodReturnArrayTypeParser;
+use RuntimeException;
 use Throwable;
 
 class GenerateResourceParserCommand extends Command
 {
-    protected $signature = 'resource-parser:generate {resourceClassName} {formatMethod}';
+    protected $signature = 'resource-parser:generate {resourceClassName} {methodName}';
 
     protected $description = 'Generate resource parser';
 
     public function handle(): int
     {
         $className = $this->argument('resourceClassName');
-        $formatMethod = $this->argument('formatMethod');
+        $methodName = $this->argument('methodName');
 
         if (!class_exists($className)) {
             $this->components->error('Class "' . $className . '" does not exist.');
             return static::FAILURE;
         }
 
-        if (!method_exists($className, $formatMethod)) {
-            $this->components->error('Class "' . $className . '" does not contain a "' . $formatMethod . '" method.');
+        if (!method_exists($className, $methodName)) {
+            $this->components->error('Class "' . $className . '" does not contain a "' . $methodName . '" method.');
             return static::FAILURE;
         }
 
         try {
             $classFile = $this->getClassFileFinder()->find($className);
 
-            $this->components->info('Found class "' . $className . '" in file "' . $classFile . '"');
-            $this->components->info(' - TODO Find "' . $formatMethod . '" and parse returns.');
+            $returns = $this->getClassMethodReturnArrayTypeParser()->parse($className, $classFile, $methodName);
+            $mergedReturns = [];
+
+            foreach ($returns as $returnGroup) {
+                foreach ($returnGroup as $property => $returnTypes) {
+                    $mergedReturns[$property] = array_unique(array_merge(
+                        $mergedReturns[$property] ?? [],
+                        $returnTypes
+                    ));
+                }
+            }
+
+            $shortClassName = class_basename($className);
+            $parserName = Str::camel($shortClassName) . Str::studly($methodName) . 'Parser';
+
+            $imports = [];
+            $shapes = [];
+
+            foreach ($mergedReturns as $property => $returnTypes) {
+                [$zodImports, $zodShapes] = $this->getZodType($returnTypes);
+                $imports = array_unique(array_merge($imports, $zodImports));
+
+                if (count($zodShapes) > 1) {
+                    $imports[] = 'union';
+                }
+
+                $shapePart = count($zodShapes) > 1
+                    ? 'union(' . implode(', ', $zodShapes) . ')'
+                    : $zodShapes[0];
+
+                $shapes[] = "    $property: $shapePart,";
+            }
+
+            $imports[] = 'object';
+            $imports[] = 'output';
+            sort($imports);
+            $imports = array_unique($imports);
+
+            $shapeText = implode(PHP_EOL, $shapes);
+            $importText = implode(', ', $imports);
+
+            $template = <<<EOT
+import {{$importText}} from 'zod';
+
+export const $parserName = object({
+{$shapeText}
+});
+
+export type $shortClassName = output<typeof $parserName>;
+
+EOT;
+
+            $this->output->write($template);
         } catch (Throwable $error) {
-            $this->components->error('Failed to parse class "' . $className . '": ' . $error->getMessage());
+            $this->components->error(
+                'Failed to generate parser for "' . $className . '": ' . $error->getMessage(),
+            );
             return static::FAILURE;
         }
 
         return static::SUCCESS;
+    }
+
+    private function getZodType(array $phpTypes): array
+    {
+        $zodImports = [];
+        $zodShapes = [];
+
+        foreach ($phpTypes as $phpType) {
+            switch ($phpType) {
+                case 'string':
+                case 'null':
+                    $zodImports[] = $phpType;
+                    $zodShapes[] = $phpType . '()';
+                    break;
+                case 'int':
+                    $zodImports[] = 'number';
+                    $zodShapes[] = 'number()';
+                    break;
+                default:
+                    throw new RuntimeException('Unhandled type "' . $phpType . '"');
+            }
+        }
+
+        return [$zodImports, $zodShapes];
     }
 
     private function getClassFileFinder(): ClassFileFinder
@@ -48,5 +128,13 @@ class GenerateResourceParserCommand extends Command
         $finder = resolve(ClassFileFinder::class);
 
         return $finder;
+    }
+
+    private function getClassMethodReturnArrayTypeParser(): ClassMethodReturnArrayTypeParser
+    {
+        /** @var ClassMethodReturnArrayTypeParser $parser */
+        $parser = resolve(ClassMethodReturnArrayTypeParser::class);
+
+        return $parser;
     }
 }
--- tests/Unit/Commands/GenerateResourceParserCommandTest.php
+++ tests/Unit/Commands/GenerateResourceParserCommandTest.php
@@ -4,35 +4,50 @@ declare(strict_types=1);
 
 namespace ResourceParserGenerator\Tests\Unit\Commands;
 
+use Closure;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\UsesClass;
 use ResourceParserGenerator\Commands\GenerateResourceParserCommand;
+use ResourceParserGenerator\DataObjects\ClassTypehints;
 use ResourceParserGenerator\Filesystem\ClassFileFinder;
+use ResourceParserGenerator\Parsers\DocBlock\ClassFileTypehintParser;
+use ResourceParserGenerator\Parsers\DocBlock\DocBlockTagTypeConverter;
+use ResourceParserGenerator\Parsers\PhpParser\ClassMethodReturnArrayTypeExtractor;
+use ResourceParserGenerator\Parsers\PhpParser\ClassMethodReturnArrayTypeLocator;
+use ResourceParserGenerator\Parsers\PhpParser\ClassMethodReturnArrayTypeParser;
+use ResourceParserGenerator\Parsers\PhpParser\ClassMethodReturnParser;
+use ResourceParserGenerator\Parsers\PhpParser\ExprObjectTypeParser;
+use ResourceParserGenerator\Parsers\PhpParser\UseStatementParser;
+use ResourceParserGenerator\Parsers\ResolveScope;
 use ResourceParserGenerator\ResourceParserGeneratorServiceProvider;
 use ResourceParserGenerator\Tests\Stubs\UserResource;
 use ResourceParserGenerator\Tests\TestCase;
+use ResourceParserGenerator\Visitors\FindArrayReturnVisitor;
+use ResourceParserGenerator\Visitors\FindClassMethodWithNameVisitor;
 
 #[CoversClass(GenerateResourceParserCommand::class)]
+#[UsesClass(ClassFileFinder::class)]
+#[UsesClass(ClassFileTypehintParser::class)]
+#[UsesClass(ClassMethodReturnArrayTypeExtractor::class)]
+#[UsesClass(ClassMethodReturnArrayTypeLocator::class)]
+#[UsesClass(ClassMethodReturnArrayTypeParser::class)]
+#[UsesClass(ClassMethodReturnParser::class)]
+#[UsesClass(ClassTypehints::class)]
+#[UsesClass(DocBlockTagTypeConverter::class)]
+#[UsesClass(ExprObjectTypeParser::class)]
+#[UsesClass(FindArrayReturnVisitor::class)]
+#[UsesClass(FindClassMethodWithNameVisitor::class)]
+#[UsesClass(ResolveScope::class)]
 #[UsesClass(ResourceParserGeneratorServiceProvider::class)]
+#[UsesClass(UseStatementParser::class)]
 class GenerateResourceParserCommandTest extends TestCase
 {
-    private const USER_RESOURCE_MOCK = '/var/www/html/app/Http/Resources/UserResource.php';
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->mock(ClassFileFinder::class)
-            ->shouldReceive('find')
-            ->with(UserResource::class)
-            ->andReturn(self::USER_RESOURCE_MOCK);
-    }
-
     public function testGeneratorShouldReturnFailureIfClassDoesNotExist(): void
     {
         $this->artisan(GenerateResourceParserCommand::class, [
             'resourceClassName' => 'ResourceParserGenerator\MissingClass',
-            'formatMethod' => 'base',
+            'methodName' => 'base',
         ])
             ->expectsOutputToContain('Class "ResourceParserGenerator\MissingClass" does not exist.')
             ->assertExitCode(1)
@@ -43,7 +58,7 @@ class GenerateResourceParserCommandTest extends TestCase
     {
         $this->artisan(GenerateResourceParserCommand::class, [
             'resourceClassName' => UserResource::class,
-            'formatMethod' => 'notARealMethod',
+            'methodName' => 'notARealMethod',
         ])
             ->expectsOutputToContain(
                 'Class "' . UserResource::class . '" does not contain a "notARealMethod" method.',
@@ -52,16 +67,40 @@ class GenerateResourceParserCommandTest extends TestCase
             ->execute();
     }
 
-    public function testGeneratorShouldReturnExpectedParser(): void
-    {
+    #[DataProvider('generatedContentProvider')]
+    public function testGeneratorShouldReturnExpectedContent(
+        string $className,
+        string $methodName,
+        Closure $outputFactory
+    ): void {
+        $template = $outputFactory->call($this);
+
         $this->artisan(GenerateResourceParserCommand::class, [
-            'resourceClassName' => UserResource::class,
-            'formatMethod' => 'authentication',
+            'resourceClassName' => $className,
+            'methodName' => $methodName,
         ])
-            ->expectsOutputToContain(
-                'Found class "' . UserResource::class . '" in file "' . self::USER_RESOURCE_MOCK . '"',
-            )
+            ->expectsOutput($template)
             ->assertExitCode(0)
             ->execute();
+    }
+
+    public static function generatedContentProvider(): array
+    {
+        return [
+            'UserResource authentication format' => [
+                'className' => UserResource::class,
+                'methodName' => 'authentication',
+                'outputFactory' => fn() => file_get_contents(
+                    dirname(__DIR__, 2) . '/Stubs/userResourceAuthenticationParser.ts.stub',
+                ),
+            ],
+            'UserResource adminList format' => [
+                'className' => UserResource::class,
+                'methodName' => 'adminList',
+                'outputFactory' => fn() => file_get_contents(
+                    dirname(__DIR__, 2) . '/Stubs/userResourceAdminListParser.ts.stub',
+                ),
+            ],
+        ];
     }
 }

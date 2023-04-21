76c523a38493641762e627017522605a47341458
Erik Perri <erik.perri@gmail.com>
2023-04-20 20:36:02 -0400

    update: Added class method return parser

---
+++ src/Parsers/PhpParser/ClassMethodReturnParser.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ResourceParserGenerator\Parsers\PhpParser;
+
+use phpDocumentor\Reflection\DocBlockFactory;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionUnionType;
+use ResourceParserGenerator\Parsers\DocBlock\ClassFileTypehintParser;
+use ResourceParserGenerator\Parsers\DocBlock\DocBlockTagTypeConverter;
+use ResourceParserGenerator\Parsers\ResolveScope;
+
+class ClassMethodReturnParser
+{
+    public function __construct(
+        private readonly DocBlockFactory $docBlockFactory,
+        private readonly DocBlockTagTypeConverter $convertDocblockTagTypes,
+        private readonly ClassFileTypehintParser $classFileTypehintParser,
+        private readonly ResolveScope $resolveScope,
+    ) {
+        //
+    }
+
+    /**
+     * @param string[] $methodsToCheck
+     * @param string $className
+     * @param string|null $classFile
+     * @return array<string, string|string[]>
+     * @throws ReflectionException
+     */
+    public function parse(array $methodsToCheck, string $className, string|null $classFile): array
+    {
+        $reflectionClass = new ReflectionClass($className);
+
+        $methods = [];
+        $classTypehints = $classFile
+            ? $this->classFileTypehintParser->parse($className, $classFile)
+            : [];
+
+        foreach ($reflectionClass->getMethods() as $method) {
+            if (!in_array($method->getName(), $methodsToCheck, true)) {
+                continue;
+            }
+
+            // Check the class typehints first since they were theoretically specified by the user as overrides
+            if (isset($classTypehints[$method->getName() . '()'])) {
+                $methods[$method->getName()] = $classTypehints[$method->getName() . '()'];
+                continue;
+            }
+
+            // Next check the docblock on the actual method
+            if ($method->getDocComment()) {
+                $docBlock = $this->docBlockFactory->create($method->getDocComment());
+
+                $return = $docBlock->getTagsByName('return');
+                $return = count($return) ? reset($return) : null;
+
+                if ($return) {
+                    $scope = $classFile
+                        ? $this->resolveScope->loadImports($classFile)
+                        : null;
+
+                    $methods[$method->getName()] = $this->convertDocblockTagTypes->convert(
+                        $return->getType(),
+                        $scope,
+                    );
+                    continue;
+                }
+            }
+
+            // Finally fall back to the actual typed return
+            $returnType = $method->getReturnType();
+            if ($returnType instanceof ReflectionUnionType) {
+                $types = [];
+                foreach ($returnType->getTypes() as $type) {
+                    $types[] = $type->getName();
+                }
+                $methods[$method->getName()] = $types;
+            } else {
+                if ($returnType) {
+                    $methods[$method->getName()] = $returnType->getName();
+                } else {
+                    $methods[$method->getName()] = ['mixed', 'void'];
+                }
+            }
+        }
+
+        return $methods;
+    }
+}
---
+++ tests/Unit/Parsers/PhpParser/ClassMethodReturnParserTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ResourceParserGenerator\Tests\Unit\Parsers\PhpParser;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+use ResourceParserGenerator\Parsers\DocBlock\ClassFileTypehintParser;
+use ResourceParserGenerator\Parsers\DocBlock\DocBlockTagTypeConverter;
+use ResourceParserGenerator\Parsers\PhpParser\ClassMethodReturnParser;
+use ResourceParserGenerator\Parsers\PhpParser\UseStatementParser;
+use ResourceParserGenerator\Parsers\ResolveScope;
+use ResourceParserGenerator\ResourceParserGeneratorServiceProvider;
+use ResourceParserGenerator\Tests\Stubs\Models\User;
+use ResourceParserGenerator\Tests\TestCase;
+
+#[CoversClass(ClassMethodReturnParser::class)]
+#[UsesClass(ClassFileTypehintParser::class)]
+#[UsesClass(DocBlockTagTypeConverter::class)]
+#[UsesClass(UseStatementParser::class)]
+#[UsesClass(ResolveScope::class)]
+#[UsesClass(ResourceParserGeneratorServiceProvider::class)]
+class ClassMethodReturnParserTest extends TestCase
+{
+    public function testParsesClassMethodReturn(): void
+    {
+        // Arrange
+        $classFile = dirname(__DIR__, 3) . '/Stubs/Models/User.php';
+
+        /**
+         * phpDocumentor uses final classes making mocking more of a pain than it's worth, so we just use make.
+         *
+         * @var ClassMethodReturnParser $parser
+         */
+        $parser = $this->app->make(ClassMethodReturnParser::class);
+
+        // Act
+        $result = $parser->parse(
+            [
+                'getRouteKey',
+                'typedMethod',
+                'typedUnionMethod',
+                'untypedWithDocMethod',
+                'untypedWithoutDocMethod',
+            ],
+            User::class,
+            $classFile,
+        );
+
+        // Assert
+        $this->assertEquals([
+            'getRouteKey' => 'string',
+            'typedMethod' => 'int',
+            'typedUnionMethod' => ['string', 'int'],
+            'untypedWithDocMethod' => 'string',
+            'untypedWithoutDocMethod' => ['mixed', 'void'],
+        ], $result);
+    }
+}

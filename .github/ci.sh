b821083a602ad34889378dacbee552fe37dd0718
Erik Perri <erik.perri@gmail.com>
2023-04-28 14:41:53 -0400

    build: Attempt at running all CI commands each run

---
+++ .github/ci.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+COMMANDS=(
+  "composer check-style"
+  "composer check-code"
+  "composer test -- --coverage-html=coverage --coverage-text --path-coverage"
+  "composer validate --strict"
+)
+
+declare -a PROCESS_IDS
+declare -a OUTPUT_FILES
+
+for COMMAND in "${COMMANDS[@]}"; do
+  OUTPUT_FILES+=("$(mktemp)")
+  $COMMAND > "${OUTPUT_FILES[-1]}" 2>&1 & PROCESS_IDS+=($!)
+done
+
+EXIT_CODE=0
+
+for ((i = 0; i < ${#PROCESS_IDS[@]}; ++i)); do
+  if ! wait "${PROCESS_IDS[i]}"; then
+    EXIT_CODE=1
+  fi
+
+  cat "${OUTPUT_FILES[i]}"
+  rm -f "${OUTPUT_FILES[i]}"
+done
+
+exit "$EXIT_CODE"

eaf8026139c788a655946cad7c9ba7227bd9dea9
Erik Perri <erik.perri@gmail.com>
2023-05-19 21:29:37 -0400

    test: Added back file generation tests

--- tests/Feature/Console/Commands/GenerateResourceParsersCommandTest.php
+++ tests/Feature/Console/Commands/GenerateResourceParsersCommandTest.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace ResourceParserGenerator\Tests\Feature\Console\Commands;
 
+use Closure;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\File;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use ResourceParserGenerator\Console\Commands\GenerateResourceParsersCommand;
 use ResourceParserGenerator\Tests\Examples\Resources\RelatedResource;
 use ResourceParserGenerator\Tests\Examples\Resources\UserResource;
@@ -84,6 +86,125 @@ class GenerateResourceParsersCommandTest extends TestCase
             ->expectsOutputToContain('Output path "/where/is/this/file" does not exist.')
             ->assertExitCode(1)
             ->execute();
+    }
+
+    #[DataProvider('generatedContentProvider')]
+    public function testGeneratorShouldReturnExpectedContent(Closure $configFactory, array $expectedOutput): void
+    {
+        $outputPath = dirname(__DIR__, 3) . '/Output';
+        $config = $configFactory->call($this, $outputPath);
+
+        Config::set('build.resource_parsers', $config);
+
+        $this->artisan(GenerateResourceParsersCommand::class)
+            ->assertExitCode(0)
+            ->execute();
+
+        foreach ($expectedOutput as $file => $contents) {
+            $this->assertEquals($contents, file_get_contents($outputPath . '/' . $file));
+        }
+    }
+
+    public static function generatedContentProvider(): array
+    {
+        $examples = dirname(__DIR__, 3) . '/Examples/Generated';
+
+        return [
+            'UserResource::adminList not configured' => [
+                'config' => fn(string $outputPath) => [
+                    'output_path' => $outputPath,
+                    'parsers' => [
+                        [UserResource::class, 'adminList'],
+                    ],
+                ],
+                'expectedOutput' => [
+                    'userResourceParsers.ts' => file_get_contents(
+                        $examples . '/UserResource-adminList-not-configured.ts.txt',
+                    ),
+                ],
+            ],
+            'UserResource::adminList configured' => [
+                'config' => fn(string $outputPath) => [
+                    'output_path' => $outputPath,
+                    'parsers' => [
+                        [
+                            'resource' => [UserResource::class, 'adminList'],
+                            'output_file' => 'custom.ts',
+                            'type' => 'CustomParser',
+                            'variable' => 'customParser',
+                        ],
+                    ],
+                ],
+                'expectedOutput' => [
+                    'custom.ts' => file_get_contents(
+                        $examples . '/UserResource-adminList-configured.ts.txt',
+                    ),
+                ],
+            ],
+            'combined' => [
+                'config' => fn(string $outputPath) => [
+                    'output_path' => $outputPath,
+                    'parsers' => [
+                        [
+                            'resource' => [UserResource::class, 'adminList'],
+                            'output_file' => 'parsers.ts',
+                        ],
+                        [
+                            'resource' => [UserResource::class, 'authentication'],
+                            'output_file' => 'parsers.ts',
+                        ],
+                        [
+                            'resource' => [UserResource::class, 'combined'],
+                            'output_file' => 'parsers.ts',
+                        ],
+                        [
+                            'resource' => [UserResource::class, 'ternaries'],
+                            'output_file' => 'parsers.ts',
+                        ],
+                        [
+                            'resource' => [UserResource::class, 'relatedResource'],
+                            'output_file' => 'parsers.ts',
+                        ],
+                        [
+                            'resource' => [RelatedResource::class, 'base'],
+                            'output_file' => 'parsers.ts',
+                        ],
+                        [
+                            'resource' => [RelatedResource::class, 'shortFormatNotNamedLikeFormatName'],
+                            'output_file' => 'parsers.ts',
+                        ],
+                        [
+                            'resource' => [RelatedResource::class, 'verbose'],
+                            'output_file' => 'parsers.ts',
+                        ],
+                    ],
+                ],
+                'expectedOutput' => [
+                    'parsers.ts' => file_get_contents($examples . '/combined.ts.txt'),
+                ],
+            ],
+            'split' => [
+                'config' => fn(string $outputPath) => [
+                    'output_path' => $outputPath,
+                    'parsers' => [
+                        [UserResource::class, 'adminList'],
+                        [UserResource::class, 'authentication'],
+                        [UserResource::class, 'combined'],
+                        [UserResource::class, 'ternaries'],
+                        [UserResource::class, 'relatedResource'],
+                        [RelatedResource::class, 'base'],
+                        [RelatedResource::class, 'shortFormatNotNamedLikeFormatName'],
+                        [RelatedResource::class, 'verbose'],
+                    ],
+                ],
+                'expectedOutput' => [
+                    'userResourceParsers.ts' => file_get_contents($examples . '/split/userResourceParsers.ts.txt'),
+                    'relatedResourceParsers.ts' => file_get_contents(
+                        $examples . '/split/relatedResourceParsers.ts.txt',
+                    ),
+                ],
+            ],
+        ];
     }
 
     private function clearTestOutputFiles(): void
